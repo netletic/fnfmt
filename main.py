439f72af3d0bd4806a8d0fd3d7198b1ae08fa2a3
Jarno Timmermans <netletic@gmail.com>
2021-11-21 16:32:06 +0000

    added docstring for fmt and fmtfile funcs

--- main.py
+++ main.py
@@ -6,11 +6,28 @@ from typing import Sequence
 
 
 def fmt(p: Path) -> list[Path]:
+    """Normalizes an entire directory if a directory is passed in.
+    Or a single file if a file is passed in.
+
+    Args:
+        p (Path): path to the file or directory
+
+    Returns:
+        list[Path]: returns a list of paths to the renamed file or files.
+    """
     files = p.glob("*") if p.is_dir() else [p]
     return [fmtfile(f) for f in files]
 
 
 def fmtfile(p: Path) -> Path:
+    """Normalizes the name of a single file.
+
+    Args:
+        p (Path): path to the file.
+
+    Returns:
+        Path: path to the renamed file.
+    """
     fn = p.name
     new_fn = fn.replace(" ", "_").lower()
     new_path = Path(p.parent / new_fn)
